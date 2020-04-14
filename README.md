b68f8e11bef4faeb4d47f8c26c2d5a381f71c144
jayant <iamjayant6@gmail.com>
2020-04-14 15:26:04 +0530

    text

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Rock Paper Scissor Game	
+
 Rock-Paper-Scissor is a very commonaly played game. Here, in this project, it is implemented using Java and Spring Boot.	
 The objective is to create a game of 50 rounds, played by 4 players viz; Player 1, Player 2, Player 3, Player 4.	
 On each round, choices of each player should be chosen at random and printed. Along with that, winnings of each player against every other player,
@@ -21,8 +22,34 @@ up till that round, should also be printed.
    - Get every player's choice of option for current round.	
    - Print player wise choices.	
    - Compare choices of each player to calculate who won from whom. For comparison, every index is compared with indexes ahead of it, starting from 0 index.	
-   - If 2 players have same choice, none of them gets point. So no operation is performed.	
-   - Else, check if second player's choice beats first player's choice. If yes, increment second player's win against first player. If no, increment first player's win against second player.	
+	 - If 2 players have same choice, none of them gets point. So no operation is performed.	
+     - Else, check if second player's choice beats first player's choice. If yes, increment second player's win against first player. If no, increment first player's win against second player.	
 In both cases, also increment respective winning player's cumulative win.	
    - Upon completion of comparison, print each player's winning against every other.	
 4. Upon completion of all rounds, print cumulative winnings of each player.
+
+## Project Files
+```
+[Game](Game.java)
+[GameController_forConsoleResponse](GameController_forConsoleResponse.java)
+[GameController2_forHtmlResponse](GameController2_forHtmlResponse.java)
+```
+
+### Output is generated in two manners
+```
+Output at Console itself - [GameController_forConsoleResponse](GameController_forConsoleResponse.java)
+Output at HTML Page (secondary, commented controller) - [GameController2_forHtmlResponse](GameController2_forHtmlResponse.java)
+```
+
+###[Game Controller - for Response in Console](GameController_forConsoleResponse.java)
+
+This controller prints output at the console itself using Core Java concepts. Various methods are used to complete required tasks.
+Each round displays the current round number, players with their chosen option, and their winnings against every other player.
+In the 2nd table for each round, each row represents the respective index player's winning against players mention as column.
+For example, for 4 players, 1st row represents 1st Player.
+Every table data corresponds to the column value specifies above.
+
+![](/images/console-output1.png)
+![](/images/console-output2.png)
+![](/images/console-output3.png)
+
--- src/main/java/com/example/demo/GameController2_forHtmlResponse.java
+++ src/main/java/com/example/demo/GameController2_forHtmlResponse.java
@@ -1,162 +1,161 @@
 package com.example.demo;
-//package com.example.demo;
-//
-//import java.util.List;
-//import java.util.Random;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class GameController2 {
-//
-////	code to chose random of rock-paper-scissor
-//	static String getRandom(List<String> list){
-//		Random r = new Random();
-//		return list.get(r.nextInt(list.size()));
-//	}
-//
-////	code to decide who wins and if draw
-//	static int whoWins(String first, String second){
-//
-//		if(first.equals("Rock")){
-//			if(second.equals("Paper"))
-//				return 0;
-//		}
-//
-//		if(first.equals("Paper")){
-//			if(second.equals("Scissor"))
-//				return 0;
-//		}
-//
-//		if(first.equals("Scissor")){
-//			if(second.equals("Rock"))
-//				return 0;
-//		}
-//
-//		return 1;
-//	}
-//	
-//	
-////	method to set player names
-//	static void SetPlayers(Game g) {
-//		String[] players = new String[g.getPlayers().length];
-//		for(int i=0; i<g.getPlayers().length; i++)
-//			players[i] = "Player " + (i+1);
-//		
-//		g.setPlayers(players);
-//	}
-//
-////	method to display player names
-//	static String DisplayPlayers(String[] players){
-//		String x = "<p style=\"text-align:center\">";
-//		for(int j=0; j<players.length; j++)
-//			x += players[j] + " | ";
-//		x += "<br>";
-//		return x;
-//	}
-//	
-//		
-////	method to display choices for current game
-//	static String DisplayChoices(String[] choices, String[] players) {
-//		String x = "<table border=1; align=center>"
-//						+ "<tr>";
-//		
-//							for (int i=0; i<players.length; i++) 
-//								x += "<th>" + players[i] + "</th>";
-//				
-//					x += "</tr>";
-//					x += "<tr>";
-//							for(int i=0; i<choices.length; i++)
-//								x += "<td>" + choices[i] + "</td>";
-//			x += "</table><br>";
-//			
-//		return x;
-//	}
-//	
-//	
-////	method to display 2D array of winnings against each player
-//	static String DisplayAgainst(int[][] against, String[] players) {
-//		String x = "<table border=1; align=center>"
-//						+ "<tr>"
-//						+ "<th></th>";
-//						
-//						for (int i=0; i<players.length; i++) 
-//							x += "<th>" + players[i] + "</th>";
-//		
-//					x += "</tr>";
-//					
-//					for(int i=0; i<against.length; i++) {
-//						x += "<tr>"
-//								+ "<th>" + players[i] + "</th>";
-//						for(int j=0; j<against.length; j++)
-//							x += "<td>" + against[i][j] + "</td>";
-//						
-//						x += "</tr>";
-//					}
-//			x += "</table><br>";
-//		return x;
-//	}
-//
-//	@RequestMapping("/start-game")
-//	@ResponseBody
-//	String startGame() {
-//		
-//		String result = "<h1 style=\"color:blue;text-align:center\">Rock-Paper-Scissor Game</h1>";
-//		
-//		int n = 4;		//Assigning number of players
-//		
-//		Game g = new Game(n);
-//
-////		Assigning each player name.
-//		SetPlayers(g);
-//		
-//
-//		for(int i=0; i<50; i++){		//Loop for each round of game.
-//
-//			result += "<h4 style=\"text-align:center\">Round " + (i+1) + "</h4>";
-//
-//			String[] currGame = new String[n];		//Array for maintaining respective player choice for each iteration in game.
-//
-////			Set player choice for current round of game.
-//			for(int j=0; j<n; j++)
-//				currGame[j] = getRandom(g.list);
-//
-//			result += DisplayChoices(currGame, g.getPlayers());
-//			
-//			
-////			Code to check winners of current round and update total-wins and wins-against-each-player.
-//			for(int j=0; j<n-1; j++){
-//				for(int k=j+1; k<n; k++){
-//					if(currGame[j]!=currGame[k]){
-//						int x = whoWins(currGame[j], currGame[k]);
-//						if(x==1){				//to update winning of j'th player -> updating upper triangular matrix
-//							g.getTotalWins()[j]++;
-//							g.getAgainst()[j][k]++;
-//						}
-//						else{					//to update winning of k'th player -> updating lower triangular matrix
-//							g.getTotalWins()[k]++;
-//							g.getAgainst()[k][j]++;							
-//						}
-//					}
-//				}
-//			}
-//
-////			Displaying winnings against each player till current round.
-//			result += DisplayAgainst(g.getAgainst(), g.getPlayers());
-//			result += "<br>";
-//
-//		}
-//
-////		Displaying total winnings of each player -> Result
-//		result += "<h2 style=\"text-align:center\">Results</h2>";
-//		result += DisplayPlayers(g.getPlayers());
-//		result += "<p style=\"text-align:center\">";
-//		for(int i=0; i<4; i++)
-//			result += g.getTotalWins()[i] + " | ";
-//		
-//		return result;
-//
-//	}
-//}
+
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController2_forHtmlResponse {
+
+//	code to chose random of rock-paper-scissor
+	static String getRandom(List<String> list){
+		Random r = new Random();
+		return list.get(r.nextInt(list.size()));
+	}
+
+//	code to decide who wins and if draw
+	static int whoWins(String first, String second){
+
+		if(first.equals("Rock")){
+			if(second.equals("Paper"))
+				return 0;
+		}
+
+		if(first.equals("Paper")){
+			if(second.equals("Scissor"))
+				return 0;
+		}
+
+		if(first.equals("Scissor")){
+			if(second.equals("Rock"))
+				return 0;
+		}
+
+		return 1;
+	}
+	
+	
+//	method to set player names
+	static void SetPlayers(Game g) {
+		String[] players = new String[g.getPlayers().length];
+		for(int i=0; i<g.getPlayers().length; i++)
+			players[i] = "Player " + (i+1);
+		
+		g.setPlayers(players);
+	}
+
+//	method to display player names
+	static String DisplayPlayers(String[] players){
+		String x = "<p style=\"text-align:center\">";
+		for(int j=0; j<players.length; j++)
+			x += players[j] + " | ";
+		x += "<br>";
+		return x;
+	}
+	
+		
+//	method to display choices for current game
+	static String DisplayChoices(String[] choices, String[] players) {
+		String x = "<table border=1; align=center>"
+						+ "<tr>";
+		
+							for (int i=0; i<players.length; i++) 
+								x += "<th>" + players[i] + "</th>";
+				
+					x += "</tr>";
+					x += "<tr>";
+							for(int i=0; i<choices.length; i++)
+								x += "<td>" + choices[i] + "</td>";
+			x += "</table><br>";
+			
+		return x;
+	}
+	
+	
+//	method to display 2D array of winnings against each player
+	static String DisplayAgainst(int[][] against, String[] players) {
+		String x = "<table border=1; align=center>"
+						+ "<tr>"
+						+ "<th></th>";
+						
+						for (int i=0; i<players.length; i++) 
+							x += "<th>" + players[i] + "</th>";
+		
+					x += "</tr>";
+					
+					for(int i=0; i<against.length; i++) {
+						x += "<tr>"
+								+ "<th>" + players[i] + "</th>";
+						for(int j=0; j<against.length; j++)
+							x += "<td>" + against[i][j] + "</td>";
+						
+						x += "</tr>";
+					}
+			x += "</table><br>";
+		return x;
+	}
+
+	@RequestMapping("/start-game")
+	@ResponseBody
+	String startGame() {
+		
+		String result = "<h1 style=\"color:blue;text-align:center\">Rock-Paper-Scissor Game</h1>";
+		
+		int n = 4;		//Assigning number of players
+		
+		Game g = new Game(n);
+
+//		Assigning each player name.
+		SetPlayers(g);
+		
+
+		for(int i=0; i<50; i++){		//Loop for each round of game.
+
+			result += "<h4 style=\"text-align:center\">Round " + (i+1) + "</h4>";
+
+			String[] currGame = new String[n];		//Array for maintaining respective player choice for each iteration in game.
+
+//			Set player choice for current round of game.
+			for(int j=0; j<n; j++)
+				currGame[j] = getRandom(g.list);
+
+			result += DisplayChoices(currGame, g.getPlayers());
+			
+			
+//			Code to check winners of current round and update total-wins and wins-against-each-player.
+			for(int j=0; j<n-1; j++){
+				for(int k=j+1; k<n; k++){
+					if(currGame[j]!=currGame[k]){
+						int x = whoWins(currGame[j], currGame[k]);
+						if(x==1){				//to update winning of j'th player -> updating upper triangular matrix
+							g.getTotalWins()[j]++;
+							g.getAgainst()[j][k]++;
+						}
+						else{					//to update winning of k'th player -> updating lower triangular matrix
+							g.getTotalWins()[k]++;
+							g.getAgainst()[k][j]++;							
+						}
+					}
+				}
+			}
+
+//			Displaying winnings against each player till current round.
+			result += DisplayAgainst(g.getAgainst(), g.getPlayers());
+			result += "<br>";
+
+		}
+
+//		Displaying total winnings of each player -> Result
+		result += "<h2 style=\"text-align:center\">Results</h2>";
+		result += DisplayPlayers(g.getPlayers());
+		result += "<p style=\"text-align:center\">";
+		for(int i=0; i<4; i++)
+			result += g.getTotalWins()[i] + " | ";
+		
+		return result;
+
+	}
+}
